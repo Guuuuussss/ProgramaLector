bc23275ffadab59b1a0f18157c783cfc81130ba3
Guuuuussss <gustavo.luna.maya@gmail.com>
2023-04-19 13:29:20 -0600

    Se modifico el codigo para validar el tipo de usuario y el horario de acceso.

--- lector2.py
+++ lector2.py
@@ -3,6 +3,7 @@ from mysql.connector import Error
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 import pyodbc
+import datetime
 
 lector = SimpleMFRC522()
 
@@ -21,8 +22,13 @@ def conectar_bd():
         return conexion
 
 def registrar_acceso():
+    # Obtener la fecha y hora actual
+    now = datetime.datetime.now()
 
-    if tipo_usuario == "P":
+    # Definir el rango de tiempo permitido como 20 minutos antes o después de la fecha y hora actual
+    time_range = datetime.timedelta(minutes=20)
+
+    if tipo_usuario == "P" and now - time_range <= horario_format <= now + time_range :
         cursor=conexion.cursor()
         query = "INSERT INTO proyecto_accesos.accesos (fecha_acceso, identificador) VALUES (CURRENT_TIMESTAMP(), %s)"
         valores = (identificador,)
@@ -30,7 +36,10 @@ def registrar_acceso():
         conexion.commit()
         cursor.close()
     else:
-        print("No se puede agregar un acceso para un usuario que no es profesor")
+        if tipo_usuario != "P":
+            print("No se puede agregar un acceso para un usuario que no es profesor.")
+        else:
+            print("No se registrara el acceso ya que lo intenta realizar fuera del horario valido.")
 
 
 try: 
@@ -43,7 +52,7 @@ try:
             registro = str(id)
             identificador = registro[1:13]
             cursor=conexion.cursor()
-            query = "SELECT * FROM proyecto_accesos.usuarios WHERE identificador = %s;"
+            query = "SELECT * FROM proyecto_accesos.usuarios LEFT JOIN clases ON usuarios.identificador = clases.id_profesor WHERE usuarios.identificador = %s;"
             
             try:
                 cursor.execute(query, (identificador,))
@@ -55,7 +64,19 @@ try:
                 matricula = resultado[4]
                 tipo_usuario = resultado[5]
                 contrasena = resultado[6]
+                id_horario = resultado[7]
+                nombre_materia = resultado[8]
+                salon = resultado[9]
+                clave_materia = resultado[10]
+                grupo = resultado[11]
+                horario = resultado[12]
+                id_profesor = resultado[13]
                 # aquí puedes hacer lo que quieras con las variables obtenidas
+
+                # Supongamos que tienes una variable "fecha_hora" con un dato tipo DATETIME de MySQL
+                horario_format = datetime.datetime.strptime(horario, '%Y-%m-%d %H:%M:%S')
+
+               
 
                 registrar_acceso()
                 
